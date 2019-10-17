e580433c5e1b88cfed5ee6cae740893f0f222de7
Thomas Schäfer <t.schaefer@headtrip.eu>
2019-10-17 16:28:09 +0200

    change host of audio files

--- src/util/audio.ts
+++ src/util/audio.ts
@@ -1,7 +1,7 @@
-export const select1 = new Audio('https://download2263.mediafire.com/0cw99osufhwg/y4hr0qm3bqh9t1m/select-1.wav')
-export const select2 = new Audio('https://download1590.mediafire.com/qe214fqkwqzg/c8sn017jsa36qpm/select-2.wav')
-export const select3 = new Audio('https://download2262.mediafire.com/a5odt0kpejtg/p6lxgsl225zyqgo/select-3.wav')
-export const selectFail = new Audio('https://download1502.mediafire.com/picn259w3x2g/m5bjehf11yg5x1q/select-fail.wav')
+export const select1 = new Audio('http://static.honybot.com/sounds/select-1.wav')
+export const select2 = new Audio('http://static.honybot.com/sounds/select-2.wav')
+export const select3 = new Audio('http://static.honybot.com/sounds/select-3.wav')
+export const selectFail = new Audio('http://static.honybot.com/sounds/select-fail.wav')
 
 export const audio = {
   select1,
@@ -12,24 +12,24 @@ export const audio = {
 
 export const playSelect1 = () => {
   try {
-    // select1.play()
+    select1.play()
   } catch (e) { }
 }
 
 export const playSelect2 = () => {
   try {
-    // select2.play()
+    select2.play()
   } catch (e) { }
 }
 
 export const playSelectSet = () => {
   try {
-    // select3.play()
+    select3.play()
   } catch (e) { }
 }
 
 export const playSelectFail = () => {
   try {
-    // selectFail.play()
+    selectFail.play()
   } catch (e) { }
 }
